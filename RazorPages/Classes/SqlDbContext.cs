fc18b9137d5f7680a89c08913387a6fdde2d6a78
Gabrial <493686444@qq.com>
2021-05-12 22:06:57 +0800

    添加了一些类

---
+++ RazorPages/Classes/SqlDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages.Pages.Classes
+{
+    public class SqlDbContext:DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connstr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyDatabase;Integrated Security=True;" +
+                "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(connstr);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- RazorPages/Pages/Entity/Content/Article.cshtml.cs
+++ RazorPages/Pages/Entity/Content/Article.cshtml.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Pages.Classes;
 
 namespace RazorPages.Pages.Entity.Content
 {
     public class ArticleModel : PageModel
     {
+        public SqlDbContext context { set; get; } = new SqlDbContext();
+        public Article article { get; set; } = new Article();
         public void OnGet()
         {
+            int id = Convert.ToInt32(Request.Query["id"][0]);
+            article = context.Articles.Find(id);
         }
     }
 }
