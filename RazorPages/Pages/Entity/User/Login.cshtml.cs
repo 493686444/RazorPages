e248c0ab5f79070678c9b027967092aea507e833
Gabrial <493686444@qq.com>
2021-05-14 21:03:24 +0800

    登录注册 调整

--- RazorPages/Pages/Entity/User/Login.cshtml.cs
+++ RazorPages/Pages/Entity/User/Login.cshtml.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using C=RazorPages.Classes;
 
 namespace RazorPages.Pages.Entity
 {
     public class LoginModel : PageModel
     {
+        public string UserName { set; get;}
+        public string PassWord { set; get; }
+        public string AuthCode { set; get; }
+       
+        public C.SqlDbContext context { set; get; }
+        public C.User User { set; get; }
         public void OnGet()
         {
+
         }
     }
 }
