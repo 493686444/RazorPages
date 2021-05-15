e04a2dc5738973a9f3056d2aea955df2e55d83f3
Gabrial <493686444@qq.com>
2021-05-15 14:02:52 +0800

    注册页面及  其model的基本逻辑  制作完成

--- RazorPages/Pages/Entity/User/Register.cshtml.cs
+++ RazorPages/Pages/Entity/User/Register.cshtml.cs
@@ -10,11 +10,21 @@ namespace RazorPages.Pages
 {
     public class RegisterModel : PageModel
     {
-        public SqlDbContext context { set; get; }
-        public User User { set; get; }
+        [BindProperty]
+        public SqlDbContext context { set; get; } = new SqlDbContext();
+        [BindProperty]
+        public User NewUser { set; get; } = new User();
         public string SecondPassword { set; get; }
         public void OnGet()
         {
+        }
+        public void OnPost() 
+        {
+            //检验两次密码的输入暂时搁置
+            NewUser.Gender = true;//测试补充的内容
+
+            context.Users.Add(NewUser);
+            context.SaveChanges();
         }
     }
 }
