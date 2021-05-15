39f3e89c0542a1b53576050b597c744a1fb3839a
Gabrial <493686444@qq.com>
2021-05-15 10:43:47 +0800

    前端代码的整理

--- RazorPages/Pages/Entity/User/Register.cshtml.cs
+++ RazorPages/Pages/Entity/User/Register.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Classes;
 
 namespace RazorPages.Pages
 {
     public class RegisterModel : PageModel
     {
+        public SqlDbContext context { set; get; }
+        public User User { set; get; }
+        public string SecondPassword { set; get; }
         public void OnGet()
         {
         }
