d4a0b9509a460a46808af46f78741bdcd63ac0a8
Gabrial <493686444@qq.com>
2021-05-20 16:04:13 +0800

    PartialView 制作完成

--- RazorPages/Pages/Article/Index.cshtml.cs
+++ RazorPages/Pages/Article/Index.cshtml.cs
@@ -11,17 +11,16 @@ namespace RazorPages.Pages.Article
 {
     public class IndexModel : PageModel
     {
-        private SqlDbContext context { set; get; } = new SqlDbContext();
-        public List<C.Article> NewArticles = new List<C.Article>();
         public int pageIndex { set; get; }
-        public List<C.Article> viewArticles { get; set; } = new List<C.Article>();
+        private SqlDbContext context { set; get; } = new SqlDbContext();
+        public List<C.Article> AllArticles = new List<C.Article>();
+        public List<C.Article> PageArticles { get; set; } = new List<C.Article>();
 
         public void OnGet()
         {
             pageIndex = Convert.ToInt32(Request.Query["pageIndex"][0]);
-            NewArticles = context.Articles.Where(a => a.Title != null).ToList();
-            viewArticles = NewArticles.Skip(pageIndex*4).Take(4).ToList();
-
+            AllArticles = context.Articles.Where(a => a.Title != null).ToList();
+            PageArticles = AllArticles.Skip(pageIndex*4).Take(4).ToList();
         }
         public void OnPost() 
         {
--- RazorPages/Pages/Suggest/Index.cshtml.cs
+++ RazorPages/Pages/Suggest/Index.cshtml.cs
@@ -4,19 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using C = RazorPages.Classes;
 
-namespace RazorPages.Pages.User
+namespace RazorPages.Pages.Suggest
 {
-    [BindProperties]
     public class IndexModel : PageModel
     {
-        public C.User User { get; set; }
         public void OnGet()
         {
-            User = new C.User();
-            User.Id = Convert.ToInt32(Request.Query["Id"][0]);
-            User.Name = "HJB";
         }
     }
 }
